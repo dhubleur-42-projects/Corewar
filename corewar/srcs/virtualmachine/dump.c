2925caceb6afda9424e6c1a0da4fd0d5bb069ebf
Onora <onora.hubleur@pm.me>
2025-08-16 17:58:40 +0200

    feat(corewar): use function to compute hexa padding

--- corewar/srcs/virtualmachine/dump.c
+++ corewar/srcs/virtualmachine/dump.c
@@ -8,6 +8,26 @@
 	((owner) == 4 ? RED : \
 	(RESET))))))
 
+int hex_len(unsigned int v) {
+    int len = 0;
+    if (v == 0) return 1;
+    while (v > 0) {
+        v /= 16;
+        len++;
+    }
+    return len;
+}
+
+const char *get_hexa_zero_padding(int value, int size) {
+	static char padding[10];
+	int len = size - hex_len(value);
+	for (int i = 0; i < len; i++) {
+		padding[i] = '0';
+	}
+	padding[len] = '\0';
+	return padding;
+}
+
 void dump_memory(const virtualmachine_t *vm, int fd) {
 	if (vm == NULL || vm->memory == NULL || vm->memory->cells == NULL) {
 		ft_dprintf(fd, "Memory is not initialized.\n");
@@ -17,14 +37,12 @@ void dump_memory(const virtualmachine_t *vm, int fd) {
 		if (i % 32 == 0) {
 			ft_dprintf(fd, "%s0x%s%x: ", 
 				PURPLE,
-				// TODO: cleaner 0 padding
-				(i < 0x10 ? "00" : i < 0x100 ? "0" : ""),
+				get_hexa_zero_padding(i, MEM_SIZE < 0x10000 ? 4 : 8),
 				i);
 		}
 		ft_dprintf(fd, "%s%s%x%s", 
 			COLOR(vm->memory->cells[i].owner),
-			// TODO: cleaner 0 padding
-			(vm->memory->cells[i].value < 0x10 ? "0" : ""),
+			get_hexa_zero_padding(vm->memory->cells[i].value, 2),
 			vm->memory->cells[i].value,
 			RESET);
 		if ((i + 1) % 32 == 0) {
