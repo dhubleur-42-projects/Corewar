6e1769044ee37109a2cf79be8b9a3cef89fd8998
Jeremy Maia <jmaia@student.42.fr>
2025-11-10 14:04:48 +0100

    Add \n to exit messages

--- asm/includes/exit_message.h
+++ asm/includes/exit_message.h
@@ -5,20 +5,20 @@
 # include "utils.h"
 
 /* Generic */
-# define EXIT_MESSAGE_MEMORY_ALLOCATION "Memory allocation error occured"
-# define EXIT_MESSAGE_OPEN_FILE "An error occured while opening file"
+# define EXIT_MESSAGE_MEMORY_ALLOCATION "Memory allocation error occured\n"
+# define EXIT_MESSAGE_OPEN_FILE "An error occured while opening file\n"
 
 /* Parse args */
 # define EXIT_MESSAGE_PROG_NAME_TOO_LONG "Program name must not exceed "STR(FILENAME_MAX_LEN)" characters\n"
 
 /* Parse header */
-# define EXIT_MESSAGE_PARSING_HEADER_VALUE_MISSING "A header value is missing"
-# define EXIT_MESSAGE_PARSING_HEADER_VALUE_NOT_QUOTED "Header values must be surrounded by '\"'"
-# define EXIT_MESSAGE_PARSING_HEADER_VALUE_MUST_END_QUOTE "Header values must end by '\"'"
-# define EXIT_MESSAGE_PARSING_HEADER_DUPLICATE_NAME "Duplicate .name instruction"
-# define EXIT_MESSAGE_PARSING_HEADER_DUPLICATE_COMMENT "Duplicate .comment instruction"
-# define EXIT_MESSAGE_PARSING_HEADER_NAME_TOO_LONG ".name value too long"
-# define EXIT_MESSAGE_PARSING_HEADER_COMMENT_TOO_LONG ".comment value too long"
-# define EXIT_MESSAGE_PARSING_HEADER_UNKNOWN_INSTRUCTION "An error occured while parsing header: unknown instruction"
+# define EXIT_MESSAGE_PARSING_HEADER_VALUE_MISSING "A header value is missing\n"
+# define EXIT_MESSAGE_PARSING_HEADER_VALUE_NOT_QUOTED "Header values must be surrounded by '\"'\n"
+# define EXIT_MESSAGE_PARSING_HEADER_VALUE_MUST_END_QUOTE "Header values must end by '\"'\n"
+# define EXIT_MESSAGE_PARSING_HEADER_DUPLICATE_NAME "Duplicate .name instruction\n"
+# define EXIT_MESSAGE_PARSING_HEADER_DUPLICATE_COMMENT "Duplicate .comment instruction\n"
+# define EXIT_MESSAGE_PARSING_HEADER_NAME_TOO_LONG ".name value too long\n"
+# define EXIT_MESSAGE_PARSING_HEADER_COMMENT_TOO_LONG ".comment value too long\n"
+# define EXIT_MESSAGE_PARSING_HEADER_UNKNOWN_INSTRUCTION "An error occured while parsing header: unknown instruction\n"
 
 #endif
